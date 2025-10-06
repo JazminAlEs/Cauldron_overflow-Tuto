a0a28bf6c93494609d97810103e9fae938cacd93
Jazmin Alvarez <jazminalvarez@MacBook-Pro-de-Jazmin.local>
2025-10-06 18:15:20 +0200

    symfonyyyy

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Hola! Soy el Controlador de las Questions');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug){
+        return new Response(sprintf(
+            'Future page to show the question "%s"!',
+            ucwords(str_replace('-',' ', $slug))
+        ));
+    }
+}
